48d6becabd97f62584d3a5d2287d078bce937f1c
Pavel Sukhanov <psuhanov77@gmail.com>
2023-06-14 14:22:53 +0300

    add client page maket

--- src/app/for-clients/page.tsx
+++ src/app/for-clients/page.tsx
@@ -1,9 +1,35 @@
 import {PageLayout} from "@/shared/PageLayout/PageLayout";
+import styles from "./page.module.css";
 
 export default function ForClients()  {
     return (
         <PageLayout>
-            <div>Страница клиента</div>
+            <div className={styles.title}>Страница клиента >></div>
+            <div className={styles.login}>
+
+                <div className={styles.loginTop}>
+                    <div className={styles.topHeaders+" "+styles.left}>
+                        Частным клиентам:
+                    </div>
+                    <div className={styles.topHeaders+" "+styles.right}>
+                        Корпоративным клиентам:
+                    </div>
+                </div>
+
+                <div className={styles.grid}>
+                    <div className={styles.lk}>Тариф частным клиентам</div>
+                    <div className={styles.lk}>Тариф корпоративным клиентам</div>
+                    <div className={styles.lk}>Тариф частным клиентам</div>
+                    <div className={styles.lk}>Тариф корпоративным клиентам</div>
+                    <div className={styles.lk}>Тариф частным клиентам</div>
+                    <div className={styles.lk}>Тариф корпоративным клиентам</div>
+                </div>
+
+
+                <div className={styles.slogan}>
+                    Наши клиенты самые довольные!!!
+                </div>
+            </div>
         </PageLayout>
     )
 }
