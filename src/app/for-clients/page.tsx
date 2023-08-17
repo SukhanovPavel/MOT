f0f310dfbae4d1dbaf348bcfe962784e8f4d2f59
Pavel Sukhanov <psuhanov77@gmail.com>
2023-08-17 12:49:52 +0300

    fix bugs

--- src/app/for-clients/page.tsx
+++ src/app/for-clients/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function ForClients()  {
     return (
         <PageLayout>
-            <div className={styles.title}>Страница клиента >></div>
+            <div className={styles.title}>Страница клиента &gt;</div>
             <div className={styles.login}>
 
                 <div className={styles.loginTop}>
