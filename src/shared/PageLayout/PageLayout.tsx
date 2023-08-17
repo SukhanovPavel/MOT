f15ab3698ec8a064a2b0efec68c8d2b2c453009e
Pavel Sukhanov <psuhanov77@gmail.com>
2023-08-17 14:23:16 +0300

    fix child type

--- src/shared/PageLayout/PageLayout.tsx
+++ src/shared/PageLayout/PageLayout.tsx
@@ -4,7 +4,7 @@ import {Header} from "@/Components/Header/Header";
 import styles from "./PageLayout.module.css";
 
 
-export const PageLayout = ({children}: React.ReactNode | React.ReactElement | React.ReactElement[]) => (
+export const PageLayout = ({children}: React.PropsWithChildren<{}>) => (
     <div className={styles._}>
 
         <Header />
